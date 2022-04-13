4ac32475afbbb3d291db4dfdb22c2d4c8c8534ba
Marco Zuñiga <marcohurtado358@gmail.com>
2022-04-12 23:32:39 -0500

    version 1.2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Weather from "./components/Weather";
 function App() {
   return (
     <div className="App">
+      {/* <img
+        className="image"
+        src="https://conceptodefinicion.de/wp-content/uploads/2020/08/clima.jpg"
+        alt=""
+      /> */}
       <Weather />
     </div>
   );
